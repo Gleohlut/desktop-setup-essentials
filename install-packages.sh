42b31ae5e8d36f2e116ecf00e5d8d71ea814759d
Gleohlut <166930736+Gleohlut@users.noreply.github.com>
2025-04-13 22:02:29 +0700

    Add files via upload

---
+++ install-packages.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+packages=(
+  # Sound
+  pipewire
+  pipewire-alsa
+  pipewire-pulse
+  wireplumber
+  helvum
+  pavucontrol
+
+  # Clipboard and screenshots
+  wl-clipboard
+  grim
+  slurp
+  swappy
+
+  # Fonts
+  otf-font-awesome
+  ttf-font-awesome
+  ttf-jetbrains-mono
+  ttf-jetbrains-mono-nerd
+  noto-fonts
+  noto-fonts-cjk
+  noto-fonts-extra
+  noto-fonts-emoji
+
+  # WM stuff
+  waybar
+  swaync
+  hyprpaper
+  hyprlock
+  kitty
+  rofi
+  hyprpolkitagent
+  xdg-desktop-portal-gtk
+  xdg-desktop-portal-hyprland
+  qt5-wayland
+  qt6-wayland
+
+  # System stuff
+  power-profiles-daemon
+  brightnessctl
+  uwsm
+  flatpak
+
+  # Printing
+  cups
+  sane
+  hplip
+  qt5
+  python-pyqt5
+
+  # File system
+  yazi
+  thunar
+  udiskie
+
+  # Gaming
+  steam
+  lutris
+  )
+
+for package in ${packages[@]}; do
+  sudo pacman -S --needed --noconfirm ${package}
+done
